15b669e255bd3d5f539ba6c0715375374417d065
Cliburn M. Solano <cliburn.solano@sonoport.com>
2014-02-06 15:19:16 +0800

    + API support for File API

--- src/lib/core/FileReader.js
+++ src/lib/core/FileReader.js
@@ -2,18 +2,22 @@
  * @author Cliburn M. Solano
  * @email cliburn.solano@sonoport.com
  * @description Read contents of file
+ * @requires Class APISupport
  */
 define(['src/lib/core/filereader/APISupport'], function(bAPISupport) {
 
-    return function(x, y) {
+    /**
+     * Gets a FileList object
+     * @param {Object} oFileList
+     */
+    return function(oFileList) {
 
+        // Check if File API is supported
         if (bAPISupport()) {
 
             console.log("yes!");
 
         }
-
-        return x * y;
 
     };
 
