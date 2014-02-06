4df7c418d7075c8a220efb692e629c1d10936bbd
Cliburn M. Solano <cliburn.solano@sonoport.com>
2014-02-06 15:09:31 +0800

    + API support for File API

--- src/lib/core/FileReader.js
+++ src/lib/core/FileReader.js
@@ -1,9 +1,20 @@
-define(function() {
-  
-     return function(x, y) {
-       
-          return x * y;  
-       
-     } 
-  
+/**
+ * @author Cliburn M. Solano
+ * @email cliburn.solano@sonoport.com
+ * @description Read contents of file
+ */
+define(['src/lib/core/filereader/APISupport'], function(bAPISupport) {
+
+    return function(x, y) {
+
+        if (bAPISupport()) {
+
+            console.log("yes!");
+
+        }
+
+        return x * y;
+
+    };
+
 });
