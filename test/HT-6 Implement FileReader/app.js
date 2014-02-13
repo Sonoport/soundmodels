e9fb2d6734b099c4571387a847d59b4c68657240
Cliburn M. Solano <cliburn.solano@sonoport.com>
2014-02-13 16:03:34 +0800

    * Updated test files

--- test/HT-6 Implement FileReader/app.js
+++ test/HT-6 Implement FileReader/app.js
@@ -1,18 +1,26 @@
 require(['src/lib/core/FileReader'], function (fr) {
 
-fr.open("https://dl.dropboxusercontent.com/u/77191118/Sin440Hz1s-Original.wav");  
-//fr.open("https://dl.dropboxusercontent.com/u/77191118/Sin440Hz1s-Marked.mp3");
+var context;
+
+window.AudioContext = window.AudioContext||window.webkitAudioContext;
+context = new AudioContext();
+    
+//fr.setContext(context);
+
+
+//fr.open("https://dl.dropboxusercontent.com/u/77191118/Sin440Hz1s-Original.wav");  
+fr.open("https://dl.dropboxusercontent.com/u/77191118/Sin440Hz1s-Marked.mp3");
   
   function handleClick(e) {
     
     // Start your engines
-    if (fr.loadFile().isLoaded()) {
+    if (fr.isLoaded()) {
       
-      var source = fr.context().createBufferSource();
+      var source = fr.getContext().createBufferSource();
 
       source.loop = true;
-      source.buffer = fr.loadFile().getBuffer();
-      source.connect(fr.context().destination);
+      source.buffer = fr.getBuffer();
+      source.connect(fr.getContext().destination);
       source.start(0);
       
     }
