4f76fdf60c6ab152038387d48c8484a40e5a55be
Yap Hong Kheng <hong.kheng@sonoport.com>
2014-02-13 17:51:54 +0800

    Updated gainNode to releaseGainNode. The buffer will automatically "stop playing" after FADE_TIME has passed. Connection of the destination node is now defined in the constructor.

--- test/test-BaseSound/main.js
+++ test/test-BaseSound/main.js
@@ -9,11 +9,33 @@ require.config({
 require(['baseSndURL','require'],
 	function(BaseSound, require) {
 		console.log("main app loaded.");
+
+		// shim layer with setTimeout fallback
+		window.requestAnimFrame = (function(){
+ 			return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+		})();
+
 		
 		// Create a new BaseSound
 		var bs = new BaseSound();
 		
 		bs.play();
 		bs.release();
-		//bs.stop(3);
+
+
+		function runTimer() {
+			console.log("timing "+bs.audioContext.currentTime);
+			requestAnimFrame(runTimer);
+			console.log(bs.isPlaying);
+			if (bs.audioContext.currentTime < 5) {
+				//break;
+			}
+			
+		}
+		//runTimer();
 });
