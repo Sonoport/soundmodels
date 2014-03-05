a5435a34ba9905dd85cf815461daf7ea7a4cabe6
Cliburn M. Solano <cliburn.solano@sonoport.com>
2014-03-05 17:15:47 +0800

    * LoopMarker class privacy fix

--- src/lib/core/LoopMarker.js
+++ src/lib/core/LoopMarker.js
@@ -17,78 +17,21 @@ define( function () {
 
         }
 
+        // Private vars
+        
+        var that = this;
         var aRightChannel_;
         var aLeftChannel_;
         var nLoopStart_ = 0;
         var nLoopEnd_ = 0;
         var nLoopLength_ = 0;
-
+        
+        // Public vars
+        
         this.PREPOSTFIX_LEN = 5000;
         this.SPIKE_THRESH = 0.5;
         this.MAX_MP3_SILENCE = 20000;
         this.SILENCE_THRESH = 0.1;
-
-        // Privilege functions
-
-        this.setRightChannel_ = function ( value ) {
-
-            aRightChannel_ = value;
-
-        };
-
-        this.getRightChannel_ = function () {
-
-            return aRightChannel_;
-
-        };
-
-        this.setLeftChannel_ = function ( value ) {
-
-            aLeftChannel_ = value;
-
-        };
-
-        this.getLeftChannel_ = function () {
-
-            return aLeftChannel_;
-
-        };
-
-        this.setLoopStart_ = function ( value ) {
-
-            nLoopStart_ = value;
-
-        };
-
-        this.getLoopStart_ = function () {
-
-            return nLoopStart_;
-
-        };
-
-        this.setLoopEnd_ = function ( value ) {
-
-            nLoopEnd_ = value;
-
-        };
-
-        this.getLoopEnd_ = function () {
-
-            return nLoopEnd_;
-
-        };
-
-        this.setLoopLength_ = function ( value ) {
-
-            nLoopLength_ = value;
-
-        };
-
-        this.getLoopLength_ = function () {
-
-            return nLoopLength_;
-
-        };
 
         // Private functions
 
@@ -99,7 +42,7 @@ define( function () {
          * @param {AudioBuffer} buffer The buffer.
          * @returns {Boolean}
          */
-        this.bLoopMarkerFound_ = function ( buffer ) {
+        var bLoopMarkerFound_ = function ( buffer ) {
 
             var startSpikePos = -1;
             var endSpikePos = -1;
@@ -116,13 +59,13 @@ define( function () {
             // Find spike near start of file
             var pos = 0;
 
-            while ( startSpikePos < 0 && pos < buffer.length && pos < this.MAX_MP3_SILENCE ) {
+            while ( startSpikePos < 0 && pos < buffer.length && pos < that.MAX_MP3_SILENCE ) {
 
-                if ( aRightChannel_[ pos ] > this.SPIKE_THRESH ) {
+                if ( aRightChannel_[ pos ] > that.SPIKE_THRESH ) {
 
                     if ( buffer.numberOfChannels > 1 ) {
 
-                        if ( aLeftChannel_[ pos ] < -this.SPIKE_THRESH ) {
+                        if ( aLeftChannel_[ pos ] < -that.SPIKE_THRESH ) {
 
                             startSpikePos = pos;
                             break;
@@ -151,13 +94,13 @@ define( function () {
             // Find spike near end of file
             pos = buffer.length - 1;
 
-            while ( endSpikePos < 0 && pos > 0 && buffer.length - pos < this.MAX_MP3_SILENCE ) {
+            while ( endSpikePos < 0 && pos > 0 && buffer.length - pos < that.MAX_MP3_SILENCE ) {
 
-                if ( aRightChannel_[ pos ] > this.SPIKE_THRESH ) {
+                if ( aRightChannel_[ pos ] > that.SPIKE_THRESH ) {
 
                     if ( buffer.numberOfChannels > 1 ) {
 
-                        if ( aLeftChannel_[ pos ] < -this.SPIKE_THRESH ) {
+                        if ( aLeftChannel_[ pos ] < -that.SPIKE_THRESH ) {
 
                             endSpikePos = pos;
                             break;
@@ -187,8 +130,8 @@ define( function () {
             if ( startSpikePos > 0 && endSpikePos > 0 && endSpikePos > startSpikePos ) {
 
                 // Compute loop start and length
-                nLoopStart_ = startSpikePos + this.PREPOSTFIX_LEN / 2;
-                nLoopEnd_ = endSpikePos - this.PREPOSTFIX_LEN / 2;
+                nLoopStart_ = startSpikePos + that.PREPOSTFIX_LEN / 2;
+                nLoopEnd_ = endSpikePos - that.PREPOSTFIX_LEN / 2;
                 nLoopLength_ = nLoopEnd_ - nLoopStart_;
 
                 return true;
@@ -209,20 +152,20 @@ define( function () {
          * @method trimSilence
          * @param {AudioBuffer} buffer The buffer to trim silence.
          */
-        this.trimSilence_ = function ( buffer ) {
+        var trimSilence_ = function ( buffer ) {
 
             nLoopEnd_ = buffer.length - 1;
 
             // Determine if mono or stereo or more than 2 channels
             if ( buffer.numberOfChannels > 1 ) {
 
-                while ( nLoopStart_ < this.MAX_MP3_SILENCE && nLoopStart_ < nLoopLength_ && Math.abs( aLeftChannel_[ nLoopStart_ ] ) < this.SILENCE_THRESH && Math.abs( aRightChannel_[ nLoopStart_ ] ) < this.SILENCE_THRESH ) {
+                while ( nLoopStart_ < that.MAX_MP3_SILENCE && nLoopStart_ < nLoopLength_ && Math.abs( aLeftChannel_[ nLoopStart_ ] ) < that.SILENCE_THRESH && Math.abs( aRightChannel_[ nLoopStart_ ] ) < that.SILENCE_THRESH ) {
 
                     nLoopStart_++;
 
                 }
 
-                while ( buffer.length - nLoopEnd_ < this.MAX_MP3_SILENCE && nLoopEnd_ > 0 && Math.abs( aLeftChannel_[ nLoopEnd_ ] ) < this.SILENCE_THRESH && Math.abs( aRightChannel_[ nLoopEnd_ ] ) < this.SILENCE_THRESH ) {
+                while ( buffer.length - nLoopEnd_ < that.MAX_MP3_SILENCE && nLoopEnd_ > 0 && Math.abs( aLeftChannel_[ nLoopEnd_ ] ) < that.SILENCE_THRESH && Math.abs( aRightChannel_[ nLoopEnd_ ] ) < that.SILENCE_THRESH ) {
 
                     nLoopEnd_--;
 
@@ -230,13 +173,13 @@ define( function () {
 
             } else {
 
-                while ( nLoopStart_ < this.MAX_MP3_SILENCE && nLoopStart_ < nLoopLength_ && Math.abs( aRightChannel_[ nLoopStart_ ] ) < this.SILENCE_THRESH ) {
+                while ( nLoopStart_ < that.MAX_MP3_SILENCE && nLoopStart_ < nLoopLength_ && Math.abs( aRightChannel_[ nLoopStart_ ] ) < that.SILENCE_THRESH ) {
 
                     nLoopStart_++;
 
                 }
 
-                while ( buffer.length - nLoopEnd_ < this.MAX_MP3_SILENCE && nLoopEnd_ > 0 && Math.abs( aRightChannel_[ nLoopEnd_ ] ) < this.SILENCE_THRESH ) {
+                while ( buffer.length - nLoopEnd_ < that.MAX_MP3_SILENCE && nLoopEnd_ > 0 && Math.abs( aRightChannel_[ nLoopEnd_ ] ) < that.SILENCE_THRESH ) {
 
                     nLoopEnd_--;
 
@@ -256,6 +199,58 @@ define( function () {
             }
 
         };
+        
+        // Public functions
+        
+        /**
+         * Detect loop markers in the audio file and create if there is none.
+         * @method detectMarkers
+         * @param {AudioBuffer} buffer
+         * @returns {AudioBuffer} The AudioBuffer with the loop marks.
+         */
+        this.detectMarkers = function ( buffer ) {
+
+            if ( !bLoopMarkerFound_( buffer ) ) {
+
+                // Loop markers not found;
+                trimSilence_( buffer );
+
+            }
+
+        };
+        
+        /**
+         * Get the end marker.
+         * @method getEndMarker
+         * @returns {Number} The end marker position. Default value is 0;
+         */
+        this.getEndMarker = function () {
+
+            return nLoopEnd_;
+
+        };
+        
+        /**
+         * Get the start marker.
+         * @method getStartMarker
+         * @returns {Number} The start marker position. Default value is 0;
+         */
+        this.getStartMarker = function () {
+
+            return nLoopStart_;
+
+        };
+
+        /**
+         * Get loop length based on the start and end markers.
+         * @method getLoopLength
+         * @returns {Number} The loop length. Default value is 0;
+         */
+        this.getLoopLength = function () {
+
+            return nLoopLength_;
+
+        };
 
     }
 
@@ -263,57 +258,7 @@ define( function () {
 
     LoopMarker.prototype = {
 
-        constructor: LoopMarker,
-
-        /**
-         * Detect loop markers in the audio file and create if there is none.
-         * @method detectMarkers
-         * @param {AudioBuffer} buffer
-         * @returns {AudioBuffer} The AudioBuffer with the loop marks.
-         */
-        detectMarkers: function ( buffer ) {
-
-            if ( !this.bLoopMarkerFound_( buffer ) ) {
-
-                //                console.log( "Loop markers not found");
-                this.trimSilence_( buffer );
-
-            }
-
-        },
-
-        /**
-         * Get the end marker.
-         * @method getEndMarker
-         * @returns {Number} The end marker position. Default value is 0;
-         */
-        getEndMarker: function () {
-
-            return this.getLoopEnd_();
-
-        },
-
-        /**
-         * Get the start marker.
-         * @method getStartMarker
-         * @returns {Number} The start marker position. Default value is 0;
-         */
-        getStartMarker: function () {
-
-            return this.getLoopStart_();
-
-        },
-
-        /**
-         * Get loop length based on the start and end markers.
-         * @method getLoopLength
-         * @returns {Number} The loop length. Default value is 0;
-         */
-        getLoopLength: function () {
-
-            return this.getLoopLength_();
-
-        }
+        constructor: LoopMarker
 
     };
 
