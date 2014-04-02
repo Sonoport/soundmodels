068b0db1748cb035da0e5695353afc77a2bc25df
Yap Hong Kheng <hong.kheng@sonoport.com>
2014-04-02 16:53:39 +0800

    Add in base template for player, grunt build for css.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,202 +2,240 @@ module.exports = function ( grunt ) {
     "use strict";
     // Project configuration.
     grunt.initConfig( {
-        // This line makes your node configurations available for use
-        pkg: grunt.file.readJSON( 'package.json' ),
-        // Define a banner
-        banner: '/*<%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %> */ \n' +
-            'console.log("   ____                           __ \\n" + "  / _____  ___ ___  ___ ___  ____/ /_\\n" + " _\\\\ \\\\/ _ \\\\/ _ / _ \\\\/ _ / _ \\\\/ __/ __/\\n" + "/___/\\\\___/_//_\\\\___/ .__\\\\___/_/  \\\\__/ \\n" + "                 /_/                 \\n" + "Hello Developer!\\n" + "Thanks for using Sonoport Dynamic Sound Library.");\n',
-        // Define files and locations
-        files: {
-            jsSrc: 'src/lib/**/*.js',
-            testSrc: 'test/**/*.js'
-        },
-        dirs: {
-            src: 'src',
-            docs: 'docs',
-            build: 'build',
-            dist: 'dist',
-            release: 'dist/release',
-            core: 'src/lib/core',
-            models: 'src/lib/models',
-            temp: 'src/lib/temp',
-        },
-        // JS Beautifier - automatic code cleanup.
-        jsbeautifier: {
-            files: [ 'package.json', 'Gruntfile.js', '<%= files.jsSrc %>' ],
-            options: {
-                config: ".jsbeautifyrc"
-            }
-        },
-        // JSHint
-        jshint: {
-            all: [ 'package.json', 'Gruntfile.js', '<%= files.jsSrc %>' ]
-        },
-        requirejs: {
-            compile: {
+            // This line makes your node configurations available for use
+            pkg: grunt.file.readJSON( 'package.json' ),
+            // Define a banner
+            banner: '/*<%= pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %> */ \n' +
+                'console.log("   ____                           __ \\n" + "  / _____  ___ ___  ___ ___  ____/ /_\\n" + " _\\\\ \\\\/ _ \\\\/ _ / _ \\\\/ _ / _ \\\\/ __/ __/\\n" + "/___/\\\\___/_//_\\\\___/ .__\\\\___/_/  \\\\__/ \\n" + "                 /_/                 \\n" + "Hello Developer!\\n" + "Thanks for using Sonoport Dynamic Sound Library.");\n',
+            // Define files and locations
+            files: {
+                jsSrc: 'src/lib/**/*.js',
+                testSrc: 'test/**/*.js'
+            },
+            dirs: {
+                src: 'src',
+                docs: 'docs',
+                build: 'build',
+                dist: 'dist',
+                release: 'dist/release',
+                core: 'src/lib/core',
+                models: 'src/lib/models',
+                temp: 'src/lib/temp',
+                player: 'jsmplayer'
+            },
+            // JS Beautifier - automatic code cleanup.
+            jsbeautifier: {
+                files: [ 'package.json', 'Gruntfile.js', '<%= files.jsSrc %>' ],
                 options: {
-                    optimize: "uglify2",
-                    baseUrl: "src/lib/",
-                    dir: "<%= dirs.build %>",
-                    modules: [ {
-                        name: 'models/Looper'
-                    }, {
-                        name: 'models/Extender'
-                    }, {
-                        name: 'models/Scrubber'
-                    }, {
-                        name: 'models/Trigger'
-                    }, {
-                        name: 'models/MultiTrigger'
-                    }, {
-                        name: 'models/Action'
-                    }, ],
-                    uglify2: {
-                        compress: {
-                            sequences: true,
-                            properties: true, // optimize property access: a["foo"] → a.foo
-                            dead_code: true, // discard unreachable code
-                            drop_debugger: true, // discard “debugger” statements
-                            unsafe: false, // some unsafe optimizations (see below)
-                            conditionals: true, // optimize if-s and conditional expressions
-                            comparisons: true, // optimize comparisons
-                            evaluate: true, // evaluate constant expressions
-                            booleans: true, // optimize boolean expressions
-                            loops: true, // optimize loops
-                            unused: true, // drop unused variables/functions
-                            hoist_funs: true, // hoist function declarations
-                            hoist_vars: false, // hoist variable declarations
-                            if_return: true, // optimize if-s followed by return/continue
-                            join_vars: true, // join var declarations
-                            cascade: true, // try to cascade `right` into `left` in sequences
-                            side_effects: true, // drop side-effect-free statements
-                            warnings: true, // warn about potentially dangerous optimizations/code
-                            global_defs: {} // global definitions
-                        },
-                        mangle: {
-                            toplevel: true
+                    config: ".jsbeautifyrc"
+                }
+            },
+            // JSHint
+            jshint: {
+                all: [ 'package.json', 'Gruntfile.js', '<%= files.jsSrc %>' ]
+            },
+            requirejs: {
+                compile: {
+                    options: {
+                        optimize: "uglify2",
+                        baseUrl: "src/lib/",
+                        dir: "<%= dirs.build %>",
+                        modules: [ {
+                            name: 'models/Looper'
+                        }, {
+                            name: 'models/Extender'
+                        }, {
+                            name: 'models/Scrubber'
+                        }, {
+                            name: 'models/Trigger'
+                        }, {
+                            name: 'models/MultiTrigger'
+                        }, {
+                            name: 'models/Action'
+                        }, ],
+                        uglify2: {
+                            compress: {
+                                sequences: true,
+                                properties: true, // optimize property access: a["foo"] → a.foo
+                                dead_code: true, // discard unreachable code
+                                drop_debugger: true, // discard “debugger” statements
+                                unsafe: false, // some unsafe optimizations (see below)
+                                conditionals: true, // optimize if-s and conditional expressions
+                                comparisons: true, // optimize comparisons
+                                evaluate: true, // evaluate constant expressions
+                                booleans: true, // optimize boolean expressions
+                                loops: true, // optimize loops
+                                unused: true, // drop unused variables/functions
+                                hoist_funs: true, // hoist function declarations
+                                hoist_vars: false, // hoist variable declarations
+                                if_return: true, // optimize if-s followed by return/continue
+                                join_vars: true, // join var declarations
+                                cascade: true, // try to cascade `right` into `left` in sequences
+                                side_effects: true, // drop side-effect-free statements
+                                warnings: true, // warn about potentially dangerous optimizations/code
+                                global_defs: {} // global definitions
+                            },
+                            mangle: {
+                                toplevel: true
+                            }
                         }
                     }
                 }
-            }
-        },
-        // Watcher for updating
-        watch: {
-            scripts: {
-                files: [ '<%= files.jsSrc %>', '<%= files.testSrc %>', 'Gruntfile.js', ],
-                tasks: [ 'dev-build' ],
-                options: {
-                    spawn: false
-                }
             },
-            docs: {
-                files: [ '<%= files.jsSrc %>', '<%= files.testSrc %>', 'Gruntfile.js', ],
-                tasks: [ 'make-doc' ],
-                options: {
-                    spawn: false
-                }
-            }
-        },
-        // YUI Documentation
-        yuidoc: {
-            dev: {
-                name: '<%= pkg.name %>',
-                description: '<%= pkg.description %>',
-                version: '<%= pkg.version %>',
-                url: '<%= pkg.homepage %>',
-                options: {
-                    paths: '<%= dirs.src %>',
-                    outdir: '<%= dirs.docs %>',
-                    linkNatives: "true"
-                }
-            },
-            release: {
-                name: '<%= pkg.description %>',
-                description: '<%= pkg.description %>',
-                version: '<%= pkg.version %>',
-                url: '<%= pkg.homepage %>',
-                logo: 'http://sonoport.com/img/Logo.png',
-                options: {
-                    // SPAudioParam.js, BaseSound.js, Envelope.js
-                    paths: [ '<%= dirs.models %>', 'src/lib/temp' ],
-                    outdir: '<%= dirs.release %>/docs',
-                    linkNatives: "true",
-                    nocode: "true"
-
-                }
-            }
-        },
-        copy: {
-            dist: {
-                files: [ {
-                    expand: true,
-                    src: [ '<%= dirs.build %>/models/*.js' ],
-                    dest: '<%= dirs.release %>/lib',
-                    filter: 'isFile',
-                    flatten: true
-                }, ]
-            },
-            temp: {
-                files: [ {
-                    expand: true,
-                    src: [ '<%= dirs.core %>/{SPAudioParam,BaseSound,Envelope}.js' ],
-                    dest: '<%= dirs.temp %>',
-                    filter: 'isFile',
-                    flatten: true
-                }, ]
-            }
-        },
-        clean: {
-            temp: [ '<%= dirs.temp %>' ],
-        },
-        usebanner: {
-            main: {
-                options: {
-                    position: 'top',
-                    banner: '<%= banner %>',
-                    linebreak: true
+            // Watcher for updating
+            watch: {
+                scripts: {
+                    files: [ '<%= files.jsSrc %>', '<%= files.testSrc %>', 'Gruntfile.js', ],
+                    tasks: [ 'dev-build' ],
+                    options: {
+                        spawn: false
+                    }
                 },
-                files: {
-                    src: [ '<%= dirs.release %>/lib/*.js' ]
-                }
-            }
-        },
-        // HTTP server for testing
-        connect: {
-            build: {
-                options: {
-                    port: 8000,
-                    base: [ '<%= dirs.build %>', 'test/models' ],
-                    open: true
+                docs: {
+                    files: [ '<%= files.jsSrc %>', '<%= files.testSrc %>', 'Gruntfile.js', ],
+                    tasks: [ 'make-doc' ],
+                    options: {
+                        spawn: false
+                    },
+                // Mainly for watching changes in the html and css files
+                playerui: {
+                    files: [ '<%= dirs.player %>/**/*.scss' ],
+                    tasks: [ 'compass:devdist' ]
+                },
+                livereload: {
+                    options: {
+                        livereload: true
+                    },
+                    files: [ '<%= dirs.player %>/**/*' ] 
                 }
             },
-            release: {
-                options: {
-                    port: 8000,
-                    base: [ '<%= dirs.release %>/lib', 'test/models' ],
-                    open: true
-                }
-            }
-        }
-    } );
-    // Load Plugins
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-    grunt.loadNpmTasks( 'grunt-contrib-watch' );
-    grunt.loadNpmTasks( 'grunt-contrib-connect' );
-    grunt.loadNpmTasks( 'grunt-contrib-copy' );
-    grunt.loadNpmTasks( 'grunt-contrib-yuidoc' );
-    grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
-    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+                // YUI Documentation
+                yuidoc: {
+                    dev: {
+                        name: '<%= pkg.name %>',
+                        description: '<%= pkg.description %>',
+                        version: '<%= pkg.version %>',
+                        url: '<%= pkg.homepage %>',
+                        options: {
+                            paths: '<%= dirs.src %>',
+                            outdir: '<%= dirs.docs %>',
+                            linkNatives: "true"
+                        }
+                    },
+                    release: {
+                        name: '<%= pkg.description %>',
+                        description: '<%= pkg.description %>',
+                        version: '<%= pkg.version %>',
+                        url: '<%= pkg.homepage %>',
+                        logo: 'http://sonoport.com/img/Logo.png',
+                        options: {
+                            // SPAudioParam.js, BaseSound.js, Envelope.js
+                            paths: [ '<%= dirs.models %>', 'src/lib/temp' ],
+                            outdir: '<%= dirs.release %>/docs',
+                            linkNatives: "true",
+                            nocode: "true"
 
-    grunt.loadNpmTasks( 'grunt-jsbeautifier' );
-    grunt.loadNpmTasks( 'grunt-banner' );
+                        }
+                    }
+                },
+                copy: {
+                    dist: {
+                        files: [ {
+                            expand: true,
+                            src: [ '<%= dirs.build %>/models/*.js' ],
+                            dest: '<%= dirs.release %>/lib',
+                            filter: 'isFile',
+                            flatten: true
+                        }, ]
+                    },
+                    temp: {
+                        files: [ {
+                            expand: true,
+                            src: [ '<%= dirs.core %>/{SPAudioParam,BaseSound,Envelope}.js' ],
+                            dest: '<%= dirs.temp %>',
+                            filter: 'isFile',
+                            flatten: true
+                        }, ]
+                    }
+                },
+                clean: {
+                    temp: [ '<%= dirs.temp %>' ],
+                },
+                usebanner: {
+                    main: {
+                        options: {
+                            position: 'top',
+                            banner: '<%= banner %>',
+                            linebreak: true
+                        },
+                        files: {
+                            src: [ '<%= dirs.release %>/lib/*.js' ]
+                        }
+                    }
+                },
+                // Player related 
+                // To run this will require Sass (3.3.4), Compass (1.0.0-alpha) and Susy (2.1.1) pre-installed.
+                // compile sass files to css using Compass: http://compass-style.org/
+                // Also depends on Susy http://susy.oddbird.net/ to generate css for grids
+                compass: {
+                    devdist: {
+                        options: {
+                            sassDir: '<%= dirs.player %>/sass',
+                            cssDir: '<%= dirs.player %>/css',
+                            require: 'susy'
+                        }
+                    }
+                },
+                // HTTP server for testing
+                connect: {
+                    build: {
+                        options: {
+                            port: 8000,
+                            base: [ '<%= dirs.build %>', 'test/models' ],
+                            open: true
+                        }
+                    },
+                    release: {
+                        options: {
+                            port: 8000,
+                            base: [ '<%= dirs.release %>/lib', 'test/models' ],
+                            open: true
+                            }
+                    }, 
+                    player: {
+                        options: {
+                            port: 8080,
+                                base: [ '<%= dirs.build %>', '<%= dirs.player %>' ],
+                                        livereload: true
+                                    }
+                            }
+                        }
+                } );
+                // Load Plugins
+                grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+                grunt.loadNpmTasks( 'grunt-contrib-watch' );
+                grunt.loadNpmTasks( 'grunt-contrib-connect' );
+                grunt.loadNpmTasks( 'grunt-contrib-copy' );
+                grunt.loadNpmTasks( 'grunt-contrib-yuidoc' );
+                grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
+                grunt.loadNpmTasks( 'grunt-contrib-clean' );
 
-    grunt.registerTask( 'dev-build', [ 'jsbeautifier', 'jshint', 'requirejs' ] );
-    grunt.registerTask( 'make-doc', [ 'jsbeautifier', 'jshint', 'yuidoc:dev' ] );
+                grunt.loadNpmTasks( 'grunt-jsbeautifier' );
+                grunt.loadNpmTasks( 'grunt-banner' );
 
-    grunt.registerTask( 'release', [ 'jshint', 'requirejs', 'copy:temp', 'yuidoc:release', 'clean:temp', 'copy:dist', 'usebanner' ] );
+                // Player related
+                grunt.loadNpmTasks( 'grunt-contrib-compass' );
 
-    grunt.registerTask( 'test', [ 'dev-build', 'connect:build', 'watch' ] );
-    grunt.registerTask( 'test-release', [ 'release', 'connect:release', 'watch' ] );
-};
+                // Player css related, probably not necessary to run this unless there is a change in design
+                grunt.registerTask( 'player-build', [ 'compass', 'connect:player', 'watch:livereload', 'watch:playerui' ] );
+
+                //grunt.registerTask('player-compile', );
+                grunt.registerTask( 'dev-build', [ 'jsbeautifier', 'jshint', 'requirejs' ] );
+                grunt.registerTask( 'make-doc', [ 'jsbeautifier', 'jshint', 'yuidoc:dev' ] );
+
+                grunt.registerTask( 'release', [ 'jshint', 'requirejs', 'copy:temp', 'yuidoc:release', 'clean:temp', 'copy:dist', 'usebanner' ] );
+
+                grunt.registerTask( 'test', [ 'dev-build', 'connect:build', 'watch' ] );
+                grunt.registerTask( 'test-release', [ 'release', 'connect:release', 'watch' ] );
+            };
---
+++ jsmplayer/js/player.js
@@ -0,0 +1,154 @@
+// main file
+
+var AudioContext = webkitAudioContext || AudioContext;
+var context = new AudioContext();
+
+$( document )
+    .ready( function () {
+        // select options dropdown menu for selecting sound models
+        $( "select[name='jsm']" )
+            .selectpicker( {
+                style: 'btn-lg btn-primary',
+                menuStyle: 'dropdown-inverse'
+            } );
+    } );
+// Test loading of Looper class
+require.config( {
+    baseUrl: "./"
+} );
+require( [ "models/Looper", "core/SPAudioParam" ], function ( Looper, SPAudioParam ) {
+
+    var surfURL = "https://dl.dropboxusercontent.com/u/2117088/ocean_edge.mp3";
+    var runURL = "https://dl.dropboxusercontent.com/u/2117088/WorkoutTrack.mp3";
+    var alienURL = "https://dl.dropboxusercontent.com/u/2117088/spaceship_11.mp3";
+
+    // Looper
+    var lp = new Looper( runURL, onLoad, null, context );
+
+    var generatedOnce = 0;
+
+    function onLoad( status ) {
+        // After sound is loaded
+        console.log( "Looper Loaded :" + status );
+        generatedOnce++;
+        // Generate the sliders on the first load
+        if ( generatedOnce === 1 ) {
+            generateParam( lp );
+        }
+    }
+    // Pass in the sound model object to tie interface to the model
+    generateInterface( lp );
+
+    function generateInterface( snd ) {
+        $( document )
+            .ready( function () {
+                // toggle sound
+                $( "#playbtn" )
+                    .click( function () {
+                        // toggle play button
+                        console.log( "play" );
+                        snd.play();
+                    } );
+                $( "#pausebtn" )
+                    .click( function () {
+                        console.log( "pause" );
+                        snd.pause();
+                    } );
+                $( "#stopbtn" )
+                    .click( function () {
+                        console.log( "stop" );
+                        snd.stop( 0 );
+                    } );
+
+                function handleFileSelect( evt ) {
+                    // Load local sources
+                    var localSources = [];
+                    // FileList object
+                    var files = evt.target.files;
+                    // files is a FileList of File objects.
+                    var output = [];
+                    for ( var i = 0; i < files.length; i++ ) {
+                        var f = files[ i ];
+                        localSources.push( f );
+                    }
+                    // Stop sound before setting source
+                    if ( snd.isPlaying ) {
+                        snd.stop();
+
+                    }
+                    snd.setSources( localSources, onLoad, null, context );
+
+                }
+
+                $( "#filebtn" )
+                    .on( 'change', handleFileSelect );
+
+                // Set Looper url sources
+                $( "#updateSource" )
+                    .on( "click", function () {
+                        // Get urls
+                        var sourceurls = $( "#fileUrls" )
+                            .val();
+                        var sources = sourceurls.split( "," );
+                        //console.log( sources, snd.isPlaying );
+                        // Stop sound before setting source
+                        if ( snd.isPlaying ) {
+                            snd.stop();
+
+                        }
+                        //console.log( "stop", lp.isPlaying );
+                        snd.setSources( sources, onLoad, null, context );
+                    } );
+
+            } );
+    }
+
+    function generateParam( snd ) {
+        // Loop through all the properties in Sound Model
+        for ( var param in snd ) {
+            var prop = snd[ param ];
+            var parameterType = Object.prototype.toString.call( prop );
+            // Get properties that are of SPAudioParam
+            if ( prop instanceof SPAudioParam ) {
+                //console.log( "props existed", param );
+                var labelnode = "<div class='param-name'><label class='label label-info param-name'>" + param + "</label></div>";
+                var slider = "<div class='pull-left param-slider'><div id='" + param + "' class='ui-slider'></div></div>";
+                var outputVal = "<div class='amount'><input type='text' id='" + param + "val' class='form-control input-sm' /></div>";
+                $( ".player-params" )
+                    .append( "<div class='param-box'>" + labelnode + slider + outputVal + "</div>" );
+                makeSlider( snd, param, prop.value, prop.minValue, prop.maxValue, 0.1 );
+            }
+        }
+
+    }
+
+    function makeSlider( snd, id, val, min, max, step ) {
+        // Make sliders
+        $( "#" + id )
+            .tickslider( {
+                min: min,
+                max: max,
+                value: snd[ id ].value,
+                slide: function ( event, ui ) {
+                    // Update input label box
+                    $( "#" + id + "val" )
+                        .val( ui.value );
+                },
+                change: function ( event, ui ) {
+                    snd[ id ].value = ui.value;
+                    console.log( ui.value, lp.playSpeed.value, snd[ id ].value );
+                }
+            } );
+        // Update text input when slider is sliding
+        $( "#" + id + "val" )
+            .val( $( "#" + id )
+                .tickslider( "value" ) );
+        // Update Slider thumb with text input
+        $( "#" + id + "val" )
+            .change( function () {
+                $( "#" + id )
+                    .tickslider( "value", this.value );
+            } );
+    }
+
+} );
