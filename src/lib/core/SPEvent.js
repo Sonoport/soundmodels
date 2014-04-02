ad045c343e728cc565e942db9fbe3b1fb11fd887
Chinmay Pendharkar <notthetup@gmail.com>
2014-04-02 18:07:19 +0800

    small error in queue event

--- src/lib/core/SPEvent.js
+++ src/lib/core/SPEvent.js
@@ -34,7 +34,7 @@ define( [],
                 };
             }
 
-            if ( typeof paramName != "undefined" || typeof paramValue != "undefined" && typeof audioBuffer !== "undefined" ) {
+            if (( typeof paramName != "undefined" || typeof paramValue != "undefined") && typeof audioBuffer !== "undefined" ) {
                 throw {
                     name: "Incorrect Parameter Type Exception",
                     message: "SPEvent can either have Parameter Information or AudioBuffer defined ",
