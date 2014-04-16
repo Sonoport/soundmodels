619092e4b5fea453ca3d1857221b42f25e3d4e4b
Chinmay Pendharkar <notthetup@gmail.com>
2014-04-16 11:54:11 +0800

    added attacked duration for start

--- src/lib/models/Looper.js
+++ src/lib/models/Looper.js
@@ -224,15 +224,21 @@ define( [ 'core/Config', 'core/BaseSound', 'core/SPAudioParam', "core/SPAudioBuf
              * @param {Number} when The delay in seconds before playing the sound
              * @param {Number} [offset] The starting position of the playhead
              * @param {Number} [duration] Duration of the portion (in seconds) to be played
+             * @param {Number} [attackDuration] Duration (in seconds) of attack ramp of the envelope.
              */
-            this.start = function ( when, offset, duration ) {
+            this.start = function ( when, offset, duration, attackDuration ) {
                 if ( !this.isPlaying ) {
                     sources_.forEach( function ( thisSource ) {
                         if ( typeof offset == 'undefined' ) {
                             offset = self.startPoint.value * thisSource.buffer.duration;
                         }
-                        //console.log( "Playing" );
                         thisSource.loop = ( self.maxLoops.value !== 1 );
+                        if ( typeof attackDuration !== 'undefined' ) {
+                            var now = this.audioContext.currentTime;
+                            this.releaseGainNode.cancelScheduledValues();
+                            this.releaseGainNode.gain.setValueAtTime( 0, now );
+                            this.releaseGainNode.gain.linearRampToValueAtTime( 1, now + attackDuration );
+                        }
                         thisSource.start( when, offset, duration );
                     } );
                 }
