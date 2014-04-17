afe9cdfb9e9bb477700abb376207b0cbbc4694ea
Yap Hong Kheng <hong.kheng@sonoport.com>
2014-04-17 15:28:55 +0800

    Update documentation of Grunt build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -243,13 +243,6 @@ module.exports = function ( grunt ) {
     grunt.loadNpmTasks( 'grunt-contrib-compass' );
     grunt.loadNpmTasks( 'grunt-bowercopy' );
 
-    // Player css related, probably not necessary to run this unless there is a change in design or you might need to install
-    // sass, compass, susy and breakpoint using gem install in ruby environment
-    grunt.registerTask( 'player-build', [ 'compass', 'jsbeautifier', 'jshint', 'connect:player', 'watch' ] );
-
-    // Run this for player testing 
-    grunt.registerTask( 'player-js', [ 'jsbeautifier', 'jshint', 'connect:player', 'watch' ] );
-
     grunt.registerTask( 'dev-build', [ 'jsbeautifier', 'jshint', 'requirejs' ] );
     grunt.registerTask( 'make-doc', [ 'jsbeautifier', 'jshint', 'yuidoc:dev' ] );
 
@@ -257,4 +250,11 @@ module.exports = function ( grunt ) {
 
     grunt.registerTask( 'test', [ 'dev-build', 'connect:build', 'watch' ] );
     grunt.registerTask( 'test-release', [ 'release', 'connect:release', 'watch' ] );
+
+    // Player css related, probably not necessary to run this unless there is a change in design or you might need to install
+    // sass, compass, susy and breakpoint using gem install in ruby environment
+    grunt.registerTask( 'player-build', [ 'compass', 'jsbeautifier', 'jshint', 'connect:player', 'watch' ] );
+
+    // Run this for player testing 
+    grunt.registerTask( 'player-js', [ 'jsbeautifier', 'jshint', 'connect:player', 'watch' ] );
 };
--- README.md
+++ README.md
@@ -37,10 +37,29 @@ Internal documentation is compiled and placed in the docs directory.
 
 Generates, with time stamps release libraries and external documentation in the dist directory.
 
-### Player build
-Install dependencies used by the Player
+### Player build for models testing
+1. Install dependencies used by the Player
 
 ``` bower install```
+2. Copy dependencies into the jsmplayer folder
+``` grunt bowercopy```
+
+3. Run player with localhost
+``` grunt player-js ```
+
+### Player design debugging
+Player's css stylesheet is using Sass to compile into a single style.css file. If there is a need to edit the css stylesheet, please install [Sass](http://www.sass-lang.com), [Compass](http://compass-style.org/), [Susy](http://susy.oddbird.net/) and [Breakpoint](http://breakpoint-sass.com/).
+
+These libraries require Ruby and they would be install as a gem. 
+
+To install:
+``` gem install sass --pre```
+``` gem install compass --pre```
+``` gem install susy --pre```
+``` gem install breakpoint```
+
+Then run this command to compile the css and localhost
+``` grunt player-build ```
 
 ## Wiki
 
