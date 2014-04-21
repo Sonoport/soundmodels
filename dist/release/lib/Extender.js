a960f78a3092729739cb7cdf314ce4f76bd2d77c
Chinmay Pendharkar <notthetup@gmail.com>
2014-04-21 16:49:26 +0800

    fixed basesound checks

--- dist/release/lib/Extender.js
+++ dist/release/lib/Extender.js
@@ -1,4 +1,4 @@
-/*javascript-sound-models - v0.4.0 - 2014-04-16 */ 
+/*javascript-sound-models - v0.4.0 - 2014-04-21 */ 
 console.log("   ____                           __ \n" + "  / _____  ___ ___  ___ ___  ____/ /_\n" + " _\\ \\/ _ \\/ _ / _ \\/ _ / _ \\/ __/ __/\n" + "/___/\\___/_//_\\___/ .__\\___/_/  \\__/ \n" + "                 /_/                 \n" + "Hello Developer!\n" + "Thanks for using Sonoport Dynamic Sound Library.");
 
 define("models/Extender",function(){});
