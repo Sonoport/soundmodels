7e6573ff1d25289b34620dc233349b24c778d341
Chinmay Pendharkar <notthetup@gmail.com>
2014-04-22 15:48:54 +0800

    added docs in release commit

---
+++ dist/release/docs/api.js
@@ -0,0 +1,25 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BaseSound",
+        "Envelope",
+        "Looper",
+        "SPAudioParam",
+        "Trigger"
+    ],
+    "modules": [
+        "Core",
+        "Models"
+    ],
+    "allModules": [
+        {
+            "displayName": "Core",
+            "name": "Core"
+        },
+        {
+            "displayName": "Models",
+            "name": "Models"
+        }
+    ]
+} };
+});
