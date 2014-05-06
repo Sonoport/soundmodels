4561e65762663e7d6eb50818a7ff66e3e338ae62
Cliburn M. Solano <cliburn.solano@sonoport.com>
2014-05-06 11:50:12 +0800

    - Old Docs

--- test/unit/cases/lib/core/test.MultiFileLoader.js
+++ test/unit/cases/lib/core/test.MultiFileLoader.js
@@ -5,7 +5,6 @@ describe('MultiFileLoader.js', function() {
   var FileLoader_ = null;
   var context = null;
   
-  
   beforeEach(function(done) {
       
     if (test1 === null) {
@@ -31,103 +30,6 @@ describe('MultiFileLoader.js', function() {
       });
     });
     
-//    it("should return false on callback function if url supplied is blank", function(done) {
-//      var a = new FileLoader_('', context, function(response){
-//        expect(response).toEqual(false);
-//        done();
-//      });
-//    });
-//    
-//    it("should return false on callback function if url supplied is not a url", function(done) {
-//      var a = new FileLoader_('abcdef', context, function(response){
-//        expect(response).toEqual(false);
-//        done();
-//      });
-//    });
-//     
-//    it("should return false on callback function if url supplied is broken", function(done) {
-//      var a = new FileLoader_('audio/doesnotexist.wav', context, function(response){
-//        expect(response).toEqual(false);
-//        done();
-//      });
-//    });
-//    
-//    xit("should be able to accept a file object", function(done) {
-//      
-//      var context = new AudioContext();
-//      var request = new XMLHttpRequest();
-//      
-//      request.open('GET', 'audio/sineloopstereo.wav', true);
-//      request.responseType = 'arraybuffer';
-//
-//      request.onload = function() {
-//          
-//        var a = new FileLoader_(request.response, context, function(response){
-//          expect(response).toEqual(true);
-//          done();
-//        });
-//
-//      };
-//      
-//      request.send();
-//      
-//    });
-    
   });
-  
-//  describe('#getBuffer', function() {
-//
-//    it("should return a buffer if file is loaded", function() {
-//      var a = test1.getBuffer();
-//      var b = Object.prototype.toString.call(a);
-//      expect(a).not.toBeNull();
-//      expect(b).toEqual('[object AudioBuffer]');
-//    });
-//
-//    it("should throw an error if no buffer is available", function() {
-//      expect(function() {
-//        test2.getBuffer();
-//      }).toThrowError();
-//    });
-//
-//  });
-//  
-//  describe('#getRawBuffer', function() {
-//
-//    it("should return the original unsliced buffer", function() {
-//      var a = test1.getBuffer();
-//      var b = Object.prototype.toString.call(a);
-//      expect(a.length).not.toEqual(b.length);
-//      expect(b).toEqual('[object AudioBuffer]');
-//    });
-//    
-//    it("should have a buffer length greater than the sliced buffer", function() {
-//      var a = test1.getBuffer();
-//      var b = test1.getRawBuffer();
-//      expect(a.length).not.toBeGreaterThan(b.length);
-//    });
-//    
-//    it("should throw an error if no buffer is available", function() {
-//      expect(function() {
-//        test2.getRawBuffer();
-//      }).toThrowError();
-//    });
-//    
-//  });
-//  
-//  describe('#isLoaded', function() {
-//
-//    it("should return true if buffer is loaded", function(done) {
-//      
-//      var test = new FileLoader_('audio/bullet.mp3', context, function(response){
-//        expect(response).toEqual(true);
-//        expect(test.isLoaded).toEqual(true);
-//        test.init("burn");
-//        done();
-//      });
-//      
-//    });
-//    
-//  });
   
 });
