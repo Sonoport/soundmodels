47ad0e0282fa7aa62810e9a1f8e0c623e4aaa527
Chinmay Pendharkar <notthetup@gmail.com>
2014-08-06 15:37:19 +0800

    removed console.log

--- src/lib/core/BaseSound.js
+++ src/lib/core/BaseSound.js
@@ -186,7 +186,7 @@ define( [ 'core/WebAudioDispatch', 'core/AudioContextMonkeyPatch' ], function ( 
             var iOS = /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
 
             function createDummyOsc() {
-                console.log( "Booting ", context );
+                //console.log( "Booting ", context );
                 bootOsc.start( 0 );
                 bootOsc.stop( context.currentTime + 0.0001 );
                 window.liveAudioContexts.push( context );
