8db69aca8871d394f69fcc98a53961861fb85e4c
Chinmay Pendharkar <notthetup@gmail.com>
2015-02-26 16:56:26 +0800

    added index for starting point

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports.Activity = require('./src/lib/models/Activity');
+module.exports.Extender = require('./src/lib/models/Extender');
+module.exports.Looper = require('./src/lib/models/Looper');
+module.exports.MultiTrigger = require('./src/lib/models/MultiTrigger');
+module.exports.Scrubber = require('./src/lib/models/Scrubber');
+module.exports.Trigger = require('./src/lib/models/Trigger');
+
+module.exports.Fader = require('./src/lib/effects/Fader');
+module.exports.Panner = require('./src/lib/effects/Panner');
+
+module.exports.SPAudioBuffer = require('./src/lib/core/SPAudioBuffer');
