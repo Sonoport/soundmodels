f7ed6fddff4f29b5720526cfbb4b7b73606505bb
Chinmay Pendharkar <notthetup@gmail.com>
2015-03-18 17:49:44 +0800

    added some reverb compatability

--- src/lib/effects/Reverb.js
+++ src/lib/effects/Reverb.js
@@ -5,6 +5,7 @@
 "use strict";
 var BaseEffect = require( '../core/BaseEffect' );
 var SPAudioParam = require( '../core/SPAudioParam' );
+var reverbGen = require( 'reverbGen' );
 var log = require( 'loglevel' );
 
 /**
@@ -29,29 +30,89 @@ function Reverb( context ) {
     this.inputNode = reverbConvolver_;
     this.outputNode = reverbConvolver_;
 
-    /**
-     * Fades or reduces the volume of the audio based on the value in percentage. 100% implies
-     * no change in volume. 0% implies completely muted audio.
-     *
-     * @property volume
-     * @type SPAudioParam
-     * @default 100
-     * @minvalue 0
-     * @maxvalue 100
-     */
-    this.registerParameter( new SPAudioParam( this, 'volume', 0, 100, 100, reverbConvolver_.gain, faderGainMap, null ), false );
+    var fadeInTime_ = 0.1;
+    var decayTime_ = 1.5;
+    var sampleRate_ = context.sampleRate;
+    var lpFreqStart_ = 15000;
+    var lpFreqEnd_ = 1000;
+    var numChannels_ = 2;
+
+    function regenIR = function (){
+        var params = {
+            fadeInTime: fadeInTime_,
+            decayTime: decayTime_,
+            sampleRate: sampleRate_,
+            lpFreqStart: lpFreqStart_,
+            lpFreqEnd: lpFreqEnd_,
+            numChannels : 2
+        }
+        reverbGen.generateReverb(params, function (buffer){
+            reverbConvolver_.buffer = buffer;
+        });
+    }
+
+    function fadeInTimeSetter (param, value){
+        fadeInTime_ = value;
+        regenIR();
+    }
+
+    function decayTimeSetter (param, value){
+        decayTime_ = value;
+        regenIR();
+    }
+
+    function lowPassStartFreqSetter (param, value){
+        lpFreqStart_ = value;
+        regenIR();
+    }
+
+    function lowPassEndFreqSetter (param, value){
+        lpFreqEnd_ = value;
+        regenIR();
+    }
 
     /**
-     * Fades or reduces the volume of the audio based on the value in decibles. 0 dB implies no
-     * change in volume. -80 dB implies almost completely muted audio.
+     * Fade-in Time.
      *
-     * @property volumeInDB
+     * @property fadeInTime
      * @type SPAudioParam
-     * @default 0
-     * @minvalue -80
-     * @maxvalue 0
+     * @default 0.1
+     * @minvalue 0.01
+     * @maxvalue 1
      */
-    this.registerParameter( new SPAudioParam( this, 'volumeInDB', -80, 0, 0, reverbConvolver_.gain, faderGainMapDB, null ), false );
+    this.registerParameter( new SPAudioParam( this, 'fadeInTime', 0.01, 1, 0.1, null, null, fadeInTimeSetter ), false );
+
+    /**
+     *
+     *
+     * @property decayTime
+     * @type SPAudioParam
+     * @default 1.5
+     * @minvalue 0.1
+     * @maxvalue 10
+     */
+    this.registerParameter( new SPAudioParam( this, 'decayTime', 0.1, 10, 1.5, null, null, decayTimeSetter ), false );
+
+    /**
+     *
+     *
+     * @property decayTime
+     * @type SPAudioParam
+     * @default 15000
+     * @minvalue 0
+     * @maxvalue 20000
+     */
+    this.registerParameter( new SPAudioParam( this, 'lowPassStartFreq', 0, 20000, 15000, null, null, lowPassStartFreqSetter), false );
+
+    /**
+     *
+     * @property decayTime
+     * @type SPAudioParam
+     * @default 1000
+     * @minvalue 0
+     * @maxvalue 20000
+     */
+    this.registerParameter( new SPAudioParam( this, 'lowPassEndFreq', 0, 20000, 1000, null, null, lowPassEndFreqSetter ), false );
 
     this.isInitialized = true;
 }
